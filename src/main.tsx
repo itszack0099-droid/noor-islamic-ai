ce47724b338c15c3d9b86b55890dfb10137cb22c
itszack0099-droid <itszack0099@gmail.com>
2026-03-12 17:44:47 +0530

    Refactor main.tsx by removing service worker code
    Removed service worker registration code and updated import path.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
-
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(() => console.log('SW registered'))
-      .catch((err) => console.log('SW failed', err));
-  });
-}
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
